14e629629695cb631a294f440ae7e2dfda03f837
Leandro Gutierrez <leandrogutierrez070@gmail.com>
2021-12-22 23:45:35 -0300

    Implement Logs middleware with winston

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,43 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as helmet from 'helmet';
+import * as winston from 'winston';
+import { utilities, WinstonModule } from 'nest-winston';
+import 'winston-daily-rotate-file';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.combine(
+            winston.format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
+            winston.format.ms(),
+            utilities.format.nestLike('Lucky', { prettyPrint: true }),
+          ),
+        }),
+        new winston.transports.DailyRotateFile({
+          level: 'info',
+          filename: 'logs/application-%DATE%.log',
+          datePattern: 'YYYY-MM-DD-HH',
+          zippedArchive: true,
+          maxSize: '20m',
+          maxFiles: '14d',
+          format: winston.format.combine(
+            winston.format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
+            winston.format.align(),
+            winston.format.errors({ stack: true }),
+            winston.format.printf(
+              (info) =>
+                `[${info.context}] ${info.level}: ${[info.timestamp]}: ${
+                  info.message
+                } - ${info.stack}`,
+            ),
+          ),
+        }),
+      ],
+    }),
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   app.enableCors();
